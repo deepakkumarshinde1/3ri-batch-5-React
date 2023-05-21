f808aa7a39d6c441e491f49ed70fcdc048ca27df
deepakkumarshinde3 <deepakkumar.shinde3@gmail.com>
2023-05-21 11:18:33 +0530

    Javascript Batch-Day-1

---
+++ main.js
@@ -0,0 +1,11 @@
+// inbuilt / native / system ==> function / object / classes
+
+// js code here
+alert(); // error ? were to check in error ? ==> console
+
+// console ==> test your in console
+console.log(); // methods
+
+//variables , opt ,
+// cond statement & opt
+// Array
